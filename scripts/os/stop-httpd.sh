87ce4ecef007a55f30ac7aa7a9ec3c04c002b9d0
SS_Dev_Team <toyosawa@sharingseed.co.jp>
2025-04-11 10:17:25 +0000

    update

--- scripts/os/stop-httpd.sh
+++ scripts/os/stop-httpd.sh
@@ -2,10 +2,14 @@ set -eu
 echo "-- $(basename $(dirname $0))/$(basename $0) --" >&2
 
 SERVICE_NAME=apache2
+if [ "$(sudo systemctl is-enabled apache2)" != "enabled" ]; then
+  echo "skip disable httpd." >&2
+else
+  sudo systemctl disable apache2
+fi
 if ! sudo systemctl is-active --quiet $SERVICE_NAME; then
   echo "skip stop httpd." >&2
 else
-  sudo systemctl disable apache2
   sudo systemctl stop apache2
 fi
 
